22ee80b8736ea17808f4ec731ce16f20a05cadcf
TheBlackFem <emmaculate.liz@gmail.com>
2022-06-23 06:42:25 -0700

    added app.py file

---
+++ app.py
@@ -0,0 +1,7 @@
+def changeme( str ):
+   "This  function prints a passed string into this function"
+   print(str)
+   return str;
+# changeme("my name is liz and am awesome")
+# changeme("and i kick ass")
+changeme("hey there")
